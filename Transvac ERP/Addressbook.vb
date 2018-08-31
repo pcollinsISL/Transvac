3eb7b351357c7d4d9da6f704365555623aac0275
Paul Collins <paul.collins@interfuture.co.uk>
2018-08-31 11:19:39 +0100

    Added Close Button to address form

--- Transvac ERP/Addressbook.vb
+++ Transvac ERP/Addressbook.vb
@@ -153,4 +153,8 @@
     Private Sub New_updDateTimePicker_ValueChanged(sender As Object, e As EventArgs) Handles New_updDateTimePicker.ValueChanged
 
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+    End Sub
 End Class
